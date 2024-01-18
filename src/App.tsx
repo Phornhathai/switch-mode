90a18c0e6b7267f67e2e8336268290efd9b3b117
Aummii Desu <74613199+Phornhathai@users.noreply.github.com>
2024-01-18 13:15:46 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Container, Grid, Paper, Typography } from "@mui/material";
+
+function App() {
+  return 
+  (
+    <div className="App">
+      
+    </div>
+
+  )
+}
+
+export default App;
